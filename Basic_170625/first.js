189a08ed98da2dd11486f60ebf8e0b2e6a93644a
Tanisha Gupta <tanishag1906@gmail.com>
2025-06-17 21:43:28 +0530

    Updated first.js with new code

--- Basic_170625/first.js
+++ Basic_170625/first.js
@@ -1,4 +1,13 @@
-
-
+//Datatypes in JavaScript
+let age=20;
+console.log(age);
+let name="Tanisha";
+console.log(name);
+let isPass=true;
+console.log(isPass);
+let a;
+console.log(a);
+let x=null;
+console.log(x);
 let h=BigInt("8");
 console.log(h);
