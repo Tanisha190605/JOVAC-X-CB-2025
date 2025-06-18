b1cca3a844692dbbbcf9fba7a95ab72611cab44f
Tanisha Gupta <167095690+Tanisha190605@users.noreply.github.com>
2025-06-18 09:48:37 +0530

    Add files via upload

---
+++ Basic_180625/practice3.js
@@ -0,0 +1,25 @@
+// let num=prompt("Enter a number: "); //user input
+// if(num%5===0){
+//     console.log("It is a multiple of 5 ");
+// }
+// else{
+//     console.log("It is not a multiple of 5");
+// }
+
+//student grades
+let score=prompt("Enter score of Student");
+if(score>=90 && score<=100){
+    console.log("Grade is: A");
+}
+else if(score>=70 && score<=89){
+    console.log("Grade is: B");
+}
+else if(score>=60 && score<=69){
+    console.log("Grade is: C");
+}
+else if(score>=50 && score<=59){
+    console.log("Grade is: D");
+}
+else{
+    console.log("Grade is: F");
+}
