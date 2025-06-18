68d0321db7b8756994283e3f93e9657359bdf71f
Tanisha Gupta <167095690+Tanisha190605@users.noreply.github.com>
2025-06-18 09:46:22 +0530

    Add files via upload

---
+++ Basic_180625/conditionals.js
@@ -0,0 +1,27 @@
+// let age=16;
+// if(age>18){
+//     console.log("You Can Vote!!");
+// }
+// else{
+//     console.log("You Cannot Vote!!");
+// }
+// let mode="light";
+// let color;
+// if(mode==="dark"){
+//     color="black";
+// }
+// else if(mode==="light"){
+//     color="white";
+// }
+// else{
+//     color="red";
+// }
+// console.log(color);
+let num=5;
+if(num%2===0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
